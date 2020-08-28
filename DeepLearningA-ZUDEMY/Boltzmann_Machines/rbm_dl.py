f3c0943a4a50cf077d3a079bf1d0fac5087ea07b
lucigrigo <lucigrigo@yahoo.com>
2020-08-28 11:20:30 +0300

    Update rbm_dl.py

--- DeepLearningA-ZUDEMY/Boltzmann_Machines/rbm_dl.py
+++ DeepLearningA-ZUDEMY/Boltzmann_Machines/rbm_dl.py
@@ -1,3 +1,4 @@
+# importing libraries
 import numpy as np
 import pandas as pd
 import torch
@@ -6,3 +7,11 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
+
+# importing dataset
+movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# preparing the training set and the test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
