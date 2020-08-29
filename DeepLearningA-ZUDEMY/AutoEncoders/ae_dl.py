2b2ed8242c144c532a655abd90a165791fb4fcd4
Grigore Lucian <lucigrigo@yahoo.com>
2020-08-29 13:15:45 +0300

    Create ae_dl.py

---
+++ DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
@@ -0,0 +1,11 @@
+# importing libraries
+import numpy as np
+import pandas as pd 
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+# importing the dataset
