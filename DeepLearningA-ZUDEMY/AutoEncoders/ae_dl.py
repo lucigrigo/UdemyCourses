a17017e3093251b0f975f13e0de4db184e6ac3bb
lucigrigo <lucigrigo@yahoo.com>
2020-09-01 16:19:17 +0300

    Update ae_dl.py

--- DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
+++ DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
@@ -20,4 +20,3 @@ training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
 training_set = np.array(training_set, dtype = 'int')
 test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
 test_set = np.array(test_set, dtype = 'int')
-
