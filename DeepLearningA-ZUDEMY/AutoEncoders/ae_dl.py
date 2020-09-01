5ffe7febbc84ea36f9d1afbd87faa6ad22ddca51
lucigrigo <lucigrigo@yahoo.com>
2020-09-01 16:19:11 +0300

    update autoencoders folder

--- DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
+++ DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
@@ -1,6 +1,6 @@
-# autoencoders
+# AutoEncoders
 
-# importing libraries
+# Importing libraries
 import numpy as np
 import pandas as pd 
 import torch
@@ -10,4 +10,14 @@ import torch.optim as optim
 import torch.utils.data
 from torch.autograd import Variable
 
-# importing the dataset
+# Importing the dataset
+movies = pd.read_csv('ml-1m/movies.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+users = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+ratings = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
+
+# Preparing the train test and the test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
+
