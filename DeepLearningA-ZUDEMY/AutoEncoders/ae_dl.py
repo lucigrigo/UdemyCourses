6e75ad1fc55a3e3bb2ef6ce78d048c3da6bd8707
Grigore Lucian <lucigrigo@yahoo.com>
2020-09-05 13:30:53 +0300

    Update ae_dl.py

--- DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
+++ DeepLearningA-ZUDEMY/AutoEncoders/ae_dl.py
@@ -65,3 +65,37 @@ criterion = nn.MSELoss()
 optimizer = optim.RMSprop(sae.parameters(), lr = 0.01, weight_decay = 0.5)
 
 # Training the SAE
+nb_epoch = 200
+for epoch in range(1, nb_epoch + 1):
+    train_loss = 0
+    s = 0.
+    for id_user in range(nb_users):
+        input = Variable(training_set[id_user]).unsqueeze(0)
+        target = input.clone()
+        if torch.sum(target.data > 0) > 0:
+            output = sae(input)
+            target.require_grad = False
+            output[target == 0] = 0
+            loss = criterion(output, target)
+            mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+            loss.backward()
+            train_loss += np.sqrt(loss.data[0] * mean_corrector)
+            s += 1.
+            optimizer.step()
+    print('epoch: ' + str(epoch) + ' train_loss: ' + str(train_loss / s))
+
+# Testing the SAE
+test_loss = 0
+s = 0.
+for id_user in range(nb_users):
+    input = Variable(training_set[id_user]).unsqueeze(0)
+    target = Variable(test_set[id_user])
+    if torch.sum(target.data > 0) > 0:
+        output = sae(input)
+        target.require_grad = False
+        output[target == 0] = 0
+        loss = criterion(output, target)
+        mean_corrector = nb_movies/float(torch.sum(target.data > 0) + 1e-10)
+        test_loss += np.sqrt(loss.data[0]*mean_corrector)
+        s += 1.
+print('test loss: '+str(test_loss/s))
