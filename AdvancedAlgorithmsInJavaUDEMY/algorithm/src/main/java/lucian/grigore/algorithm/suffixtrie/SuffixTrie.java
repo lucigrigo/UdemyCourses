b2f04d8a0f58f7421b51eaa090dfcdb83543b360
Grigore Lucian <lucigrigo@yahoo.com>
2020-09-25 13:11:38 +0300

    Update SuffixTrie.java

--- AdvancedAlgorithmsInJavaUDEMY/algorithm/src/main/java/lucian/grigore/algorithm/suffixtrie/SuffixTrie.java
+++ AdvancedAlgorithmsInJavaUDEMY/algorithm/src/main/java/lucian/grigore/algorithm/suffixtrie/SuffixTrie.java
@@ -1,10 +1,24 @@
 package lucian.grigore.algorithm.suffixtrie;
 
+import java.util.List;
+
 public class SuffixTrie {
 
     TrieNode root = new TrieNode();
 
     SuffixTrie(String text) {
         root.insertSuffix(text);
+    }
+
+    public List<Integer> search(String pattern) {
+        return root.search(pattern);
+    }
+
+    public boolean isSuffix(String pattern) {
+        return root.isSuffix(pattern);
+    }
+
+    public boolean isSubstring(String pattern) {
+        return root.isSubstring(pattern);
     }
 }
